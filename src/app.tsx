09ad44729152fc5140cbd73729ffa4eeda30b298
adammmertel <mertel.adam@gmail.com>
2020-04-18 14:09:14 +0200

    Assign random heights to the plane

--- src/app.tsx
+++ src/app.tsx
@@ -5,42 +5,37 @@ import { Canvas, useFrame, useThree } from "react-three-fiber";
 import Box from "./box";
 import Plane from "./plane";
 
-/*
 function Rig({ mouse }) {
   const { camera } = useThree();
   useFrame(() => {
-    camera.position.x += (mouse.current[0] / 50 - camera.position.x) * 0.05;
-    camera.position.y += (-mouse.current[1] / 50 - camera.position.y) * 0.05;
+    camera.position.x += (mouse.current[0] / 20 - camera.position.x) * 0.25;
+    camera.position.y +=
+      (-mouse.current[1] / 20 - camera.position.y) * 0.25 - 5;
     camera.lookAt(0, 0, 0);
   });
   return null;
 }
-*/
 
 const App: React.FC = () => {
-  const mouse = useRef([0, 0]);
+  const mouse = useRef([0, -20]);
   return (
     <Canvas
-      camera={{ position: [0, -20, 10] }}
-      /*
+      camera={{ position: [0, -20, 30], fov: 120 }}
       onMouseMove={(e) =>
         (mouse.current = [
           e.clientX - window.innerWidth / 2,
           e.clientY - window.innerHeight / 2,
         ])
       }
-      */
     >
-      >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Plane position={[0, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <pointLight intensity={0.2} position={[0, 0, 50]} />
+      <Plane position={[-25, -25, 0]} />
+      <Box position={[0, 0, 20]} />
+      <Rig mouse={mouse} />
     </Canvas>
   );
 };
 /*
 <Box position={[1.2, -2, 0]} />
-<Rig mouse={mouse} />
 */
 export default App;
--- src/plane.tsx
+++ src/plane.tsx
@@ -1,11 +1,27 @@
 import React, { useRef, useState } from "react";
 import { useFrame } from "react-three-fiber";
+import { Vector3 } from "three";
+
+const r = () => Math.random();
 
 const Plane = (props) => {
+  const vectors = [];
+  [...Array(50).keys()].forEach((x) => {
+    [...Array(50).keys()].forEach((y) => {
+      vectors.push(new Vector3(x, y, 5 - r() * 10));
+    });
+  });
+
+  console.log(vectors);
+
   return (
     <mesh {...props}>
-      <planeGeometry attach="geometry" args={[5, 5, 5, 5]} />
-      <meshStandardMaterial attach="material" color={"blue"} />
+      <planeGeometry
+        attach="geometry"
+        args={[50, 50, 49, 49]}
+        vertices={vectors}
+      />
+      <meshPhongMaterial attach="material" color={"grey"} wireframe={true} />
     </mesh>
   );
 };
