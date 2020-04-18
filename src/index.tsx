5a4b40d880922e7493975c5239259e2a6f82b59a
adammmertel <mertel.adam@gmail.com>
2020-04-18 11:31:27 +0200

    Create basic structure and functionality

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+console.log("started");
+import React from "react";
+import { render } from "react-dom";
+
+import App from "./app";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
