89c2c97c56c572a3b2fd476b0be9eafdd93a9266
adammmertel <mertel.adam@gmail.com>
2020-08-11 18:29:40 +0200

    Refactor data parsing

--- parsedata.js
+++ parsedata.js
@@ -1,35 +1,38 @@
 const fs = require("fs");
 const GeoTIFF = require("geotiff");
 
-const w = 200;
-const h = 100;
-const getPixel = (vals, x, y) => {
-  return vals[y * w + x];
+const getPixel = (values, x, y, w, h) => {
+  return parseInt(values[y * w + x]);
 };
 
 const load = async () => {
-  const tiff = await GeoTIFF.fromFile("./data/005.tiff");
-
+  const tiff = await GeoTIFF.fromFile("./data/reproject-0.01.tif");
   const image = await tiff.getImage();
+
+  //console.log(image.fileDirectory);
+  const w = image.fileDirectory.ImageWidth;
+  const h = image.fileDirectory.ImageLength;
+  console.log(w, h);
+
   const [values] = await image.readRasters({
     width: w,
     height: h,
     resampleMethod: "bilinear",
   });
 
-  const arr = [];
+  const imageArr = [];
 
   for (let x = 0; x !== w; x++) {
-    if (!arr[x]) {
-      arr[x] = [];
+    if (!imageArr[x]) {
+      imageArr[x] = [];
     }
     for (let y = 0; y !== h; y++) {
-      const px = getPixel(values, x, y);
-      arr[x][y] = px < 0 ? false : px;
+      const px = getPixel(values, x, y, w, h);
+      imageArr[x][y] = px < 0 ? false : px;
     }
   }
-  console.log(arr);
-  fs.writeFileSync("data/svk.json", JSON.stringify(arr));
+  //console.log(imageArr);
+  fs.writeFileSync("data/svk.json", JSON.stringify(imageArr));
 };
 
 load();
