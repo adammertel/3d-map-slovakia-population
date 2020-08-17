39eccc12623aeeb998c71bbb94273f9b49505f0f
adammmertel <mertel.adam@gmail.com>
2020-08-17 16:52:35 +0200

    Fix text positioning

--- src/camera.tsx
+++ src/camera.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useFrame, useThree } from "react-three-fiber";
+import * as THREE from "three";
 import { MapControls } from "three/examples/jsm/controls/OrbitControls";
 
-const minZPosition = 20;
+const minYPosition = 20;
 const keyPanningSpeed = 1;
 
 const Camera = ({}) => {
@@ -10,18 +11,33 @@ const Camera = ({}) => {
 
   const [keyDown, setKeyDown] = useState(false);
 
+  useEffect(() => {
+    console.log("did mount");
+  }, []);
+
   useFrame(() => {
-    if (camera.position.z < minZPosition) {
-      camera.position.z = minZPosition;
+    /*
+    console.log(
+      camera.rotation.y.toPrecision(2),
+      camera.rotation.z.toPrecision(2)
+    );
+    */
+    //console.log(camera.toJSON());
+
+    if (camera.position.y < minYPosition) {
+      camera.position.y = minYPosition;
     }
+
+    //camera.rotation.x = 0;
+    //camera.rotation.y = 0;
 
     //left
     if (keyDown === 37) {
-      camera.position.x -= keyPanningSpeed;
+      //camera.position.x -= keyPanningSpeed;
     }
     //right
     if (keyDown === 39) {
-      camera.position.x += keyPanningSpeed;
+      //camera.position.x += keyPanningSpeed;
     }
     //up
     if (keyDown === 38) {
@@ -41,22 +57,34 @@ const Camera = ({}) => {
     document.addEventListener("keyup", (e) => {
       setKeyDown(false);
     });
+    console.log("setting camera");
+
+    //camera.lookAt(new THREE.Vector3(300, 0, 0));
 
     const controls = new MapControls(camera, gl.domElement);
+    controls.enableDamping = false; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
 
-    controls.screenSpacePanning = true;
-    controls.autoRotate = true;
+    controls.screenSpacePanning = false;
+    controls.autoRotate = false;
     controls.panSpeed = 1.5;
     controls.zoomSpeed = 0.5;
 
-    controls.minDistance = 100;
-    controls.maxDistance = 5000;
+    controls.minDistance = 5;
+    controls.maxDistance = 500;
     controls.enableKeys = false;
 
-    controls.minPolarAngle = Math.PI * 0.6;
+    //controls.minPolarAngle = Math.PI * 0.05;
     controls.maxPolarAngle = Math.PI * 0.95;
-    controls.minAzimuthAngle = Math.PI * 0;
-    controls.maxAzimuthAngle = Math.PI * 0;
+    // controls.minAzimuthAngle = Math.PI * 0;
+    // controls.maxAzimuthAngle = Math.PI * 0;
+
+    camera.position.z = -200;
+    camera.position.y = 300;
+    camera.position.x = 0;
+
+    //camera.lookAt(0, 0, 0);
+    controls.update();
 
     return () => {
       controls.dispose();
--- src/text.tsx
+++ src/text.tsx
@@ -18,15 +18,14 @@ type TextProps = {
 const Text: React.FC = ({
   children,
   size = 1,
-  letterSpacing = 0.01,
+  letterSpacing = 0.1,
   color = "#000000",
-  centerX = true,
-  centerY = true,
+  centerX = false,
+  centerY = false,
   rotation = [0, 0, 0],
   position = [0, 0, 0],
 }: TextProps) => {
   const [font] = useState(() => new THREE.FontLoader().parse(fontFile));
-  console.log(rotation);
 
   const { camera } = useThree();
 
@@ -46,7 +45,7 @@ const Text: React.FC = ({
       letters
         .map((letter) => {
           if (letter === " ") {
-            x += size / 2;
+            x -= size / 2;
             return null;
           } else {
             const geom = new THREE.ShapeGeometry(
@@ -55,10 +54,13 @@ const Text: React.FC = ({
             geom.computeBoundingBox();
 
             const mesh = new THREE.Mesh(geom, mat);
-            mesh.position.x = x;
+            mesh.position.x = -x;
 
-            x += geom.boundingBox.max.x + letterSpacing;
-            y = Math.max(y, geom.boundingBox.max.y);
+            //mesh.rotation.x = Math.PI * 1;
+            //mesh.rotation.z = Math.PI * 1;
+
+            x -= geom.boundingBox.max.x + letterSpacing;
+            y = -Math.max(y, geom.boundingBox.max.y);
             return mesh;
           }
         })
@@ -68,7 +70,10 @@ const Text: React.FC = ({
   }, [font, children, size, letterSpacing, color]);
 
   useFrame(() => {
-    textRef.current.rotation.x = camera.rotation.x;
+    const cameraZ = camera.rotation.z;
+
+    textRef.current.rotation.y = cameraZ;
+    textRef.current.rotation.z = -Math.PI * 0.5;
   });
 
   return (
