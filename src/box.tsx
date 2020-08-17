fc2cb839d953a8da6575aeb1316ffd824d6dda2e
adammmertel <mertel.adam@gmail.com>
2020-08-17 09:35:28 +0200

    Apply autobus font

--- src/box.tsx
+++ src/box.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { useFrame } from "react-three-fiber";
 
-const Box = ({ position, sizes, keyValue }) => {
+const Box = ({ position, sizes }) => {
   // This reference will give us direct access to the mesh
   //const mesh = useRef();
 
@@ -13,7 +13,6 @@ const Box = ({ position, sizes, keyValue }) => {
     <mesh
       position={position}
       //ref={mesh}
-      key={keyValue}
       //onClick={(e) => setActive(!active)}
       //onPointerOver={(e) => setHover(true)}
       //onPointerOut={(e) => setHover(false)}
